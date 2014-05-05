ee58bebae524ba57a08c75b230367c7c4212a764
Martin Spiess <martin@bioz5-msimac.fritz.box>
2014-05-05 21:23:06 +0200

    Initial Commit

---
+++ UITableViewController Challenge Solution/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Martin Spiess on 5.5.14.
+//  Copyright (c) 2014 Molecular Samples. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/MSViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Martin Spiess on 5.5.14.
+//  Copyright (c) 2014 Molecular Samples. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSViewController : UIViewController
+
+@end
